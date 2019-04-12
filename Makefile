4109b11531987a51e6c89bc5249ac9574ff1333e
leic25 <lea.bela.97@gmail.com>
2019-04-12 13:06:39 +0200

    Dodata je ravan na kojoj ce se nalaziti objekti i igrac-kutija

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 PROGRAM = brainBox
 CC      = gcc
-CFLAGS  = -g -ansi -Wall -I/usr/X11R6/include -I/usr/pkg/include
+CFLAGS  = -g -Wall -I/usr/X11R6/include -I/usr/pkg/include
 LDFLAGS = -L/usr/X11R6/lib -L/usr/pkg/lib
-LDLIBS  = -lglut -lGLU -lGL
+LDLIBS  = -lglut -lGLU -lGL -lm
 
 $(PROGRAM): main.o
 	$(CC) $(LDFLAGS) -o $(PROGRAM) main.o $(LDLIBS)
--- main.c
+++ main.c
@@ -1,32 +1,50 @@
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <math.h>
 
-/* Dimenzije prozora */
-static int window_width, window_height;
+/* Definisemo granice parametara povrsi */
+#define U_FROM -50
+#define V_FROM -50
+#define U_TO 50
+#define V_TO 50
+
+
+
+/* Definisemo makro koji vraca maksimalnu vrednost */
+#define max(A, B) (((A) > (B)) ? (A) : (B))
 
 /* Deklaracije callback funkcija. */
+static void on_keyboard(unsigned char key, int x, int y);
 static void on_reshape(int width, int height);
 static void on_display(void);
 
-int main(int argc, char **argv)
-{
+
+static int active_function;     /* identifikator aktuelne funkcije */
+
+int main(int argc, char **argv){
+    /* Inicijalizacija promenljivih */
+    active_function = 0;
+
     /* Inicijalizuje se GLUT. */
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
 
     /* Kreira se prozor. */
-    glutInitWindowSize(800, 800);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(300, 50);
     glutCreateWindow(argv[0]);
 
     /* Registruju se callback funkcije. */
+    glutKeyboardFunc(on_keyboard);
     glutReshapeFunc(on_reshape);
     glutDisplayFunc(on_display);
 
     /* Obavlja se OpenGL inicijalizacija. */
-    glClearColor(236.0/255, 201.0/255, 63.0/255, 0);
+    glClearColor(0, 0, 0, 0);
     glEnable(GL_DEPTH_TEST);
-    glLineWidth(1);
+
+    /* Ukljucujemo normalizaciju vektora normala */
+    glEnable(GL_NORMALIZE);
 
     /* Program ulazi u glavnu petlju. */
     glutMainLoop();
@@ -34,56 +52,140 @@ int main(int argc, char **argv)
     return 0;
 }
 
-
-static void on_reshape(int width, int height)
-{
-    /* Pamte se sirina i visina prozora. */
-    window_width = width;
-    window_height = height;
+static void on_keyboard(unsigned char key, int x, int y){
+    switch (key) {
+    case 27:
+        /* Zavrsava se program. */
+        exit(0);
+        break;
+        
+    }
 }
 
-static void on_display(void)
-{
-    /* Brise se prethodni sadrzaj prozora. */
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+static void on_reshape(int width, int height){
     /* Podesava se viewport. */
-    glViewport(0, 0, window_width, window_height);
+    glViewport(0, 0, width, height);
 
     /* Podesava se projekcija. */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glFrustum(-1, 1, -1, 1, 1, 10);
+    gluPerspective(60, (float) width / height, 1, 10);
+}
+
+
+
+/* Izracunava vrednost funkcije za parametre u i v */
+float function(float u, float v){
+    return 0;
+}
+
+/* Postavlja normalu i kordinate verteksa za tacku sa pparametrima
+ * u i v */
+void set_vertex_and_normal(float u, float v){
+    float diff_u, diff_v;
+    diff_u = 0;
+    diff_v = 0;
+    
+    /* Postavljamo normalu - vektor normalan na tangentnu ravan */
+    /* Racuna se priblizan vektor normale: */
+    glNormal3f(-diff_u, 1, -diff_v);
+
+    /* Postavljamo verteks */
+    /*glVertex3f(u, function(u, v), v);  ako treba fja*/ 
+    glVertex3f(u, 0, v);
+    
+}
+
+/* Crtamo funkciju */
+void plot_function(){
+    float scale;
+    int u, v;
+
+    glPushMatrix();
+
+    /* Racunamo faktor skaliranja tako da cela funkcija bude prikazana */
+    scale = 2.0 / max( U_TO - U_FROM, V_TO - V_FROM );
+    glScalef(scale, scale, scale);
+
+    /* Crtamo funkciju strip po strip */
+    for (u = U_FROM; u < U_TO; u++) {
+        glBegin(GL_TRIANGLE_STRIP);
+        for (v = V_FROM; v <= V_TO; v++) {
+            set_vertex_and_normal(u, v);
+            set_vertex_and_normal(u + 1, v);
+        }
+        glEnd();
+    }
+    glPopMatrix();
+}
+
+static void on_display(void){
+    /* Pozicija svetla (u pitanju je direkcionalno svetlo). */
+    GLfloat light_position[] = { 1, 1, 1, 0 };
+    /* Ambijentalna boja svetla. */
+    GLfloat light_ambient[] = { 0.1, 0.1, 0.1, 1 };
+    /* Difuzna boja svetla. */
+    GLfloat light_diffuse[] = { 0.7, 0.7, 0.7, 1 };
+    /* Spekularna boja svetla. */
+    GLfloat light_specular[] = { 0.9, 0.9, 0.9, 1 };
+
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.3, 0.8, 0.3, 1 };
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0.2, 0.5, 0.5, 1 };
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 1, 0, 0, 1 };
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 30;
+
+    /* Brise se prethodni sadrzaj prozora. */
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     /* Podesava se vidna tacka. */
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(-1, 1, 1, 0, 0, 0, 0, 1, 0);
+    gluLookAt(-1.5, 1, 1.8, 0, 0, 0, 0, 1, 0);
 
-   
-        /*X*/
-        
+    /* Ukljucuje se osvjetljenje i podesavaju parametri svetla. */
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+    
+    /*X*/
     glColor3f(1,0,0);
         glBegin(GL_LINES);
             glVertex3f(0, 0, 0);
             glVertex3f(100, 0, 0);
         glEnd();
-        /*Y*/
-        
+    /*Y*/ 
     glColor3f(0,1,0);
         glBegin(GL_LINES);
             glVertex3f(0, 0, 0);
             glVertex3f(0, 100, 0);
         glEnd();
         
-        /*Z*/
-        
+    /*Z*/
     glColor3f(0,0,1);
         glBegin(GL_LINES);
             glVertex3f(0, 0, 0);
             glVertex3f(0, 0, 100);
         glEnd();
-    
-    
+
+    /* Kreira se objekat. */
+    plot_function();
+
+    /* Nova slika se salje na ekran. */
     glutSwapBuffers();
 }
+
