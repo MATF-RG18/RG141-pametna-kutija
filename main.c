3fadaa2c4309031865476a6f765bdcb7c3cbc9a3
leic25 <lea.bela.97@gmail.com>
2019-04-27 21:56:39 +0200

    izmene na ravni, dodata kocka koja predstavlja igraca, i jos neke fje

--- main.c
+++ main.c
@@ -1,30 +1,19 @@
-#include <stdlib.h>
-#include <GL/glut.h>
-#include <math.h>
-
-/* Definisemo granice parametara povrsi */
-#define U_FROM -50
-#define V_FROM -50
-#define U_TO 50
-#define V_TO 50
-
-
-
-/* Definisemo makro koji vraca maksimalnu vrednost */
-#define max(A, B) (((A) > (B)) ? (A) : (B))
+#include<stdio.h>
+#include<stdlib.h>
+#include<GL/glut.h>
+#include<math.h>
 
 /* Deklaracije callback funkcija. */
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_reshape(int width, int height);
 static void on_display(void);
 
+/*static int active_function;     identifikator aktuelne funkcije */
 
-static int active_function;     /* identifikator aktuelne funkcije */
+static float anim_param1 = 0;
+static float anim_param2 = 0;
 
 int main(int argc, char **argv){
-    /* Inicijalizacija promenljivih */
-    active_function = 0;
-
     /* Inicijalizuje se GLUT. */
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
@@ -54,10 +43,46 @@ int main(int argc, char **argv){
 
 static void on_keyboard(unsigned char key, int x, int y){
     switch (key) {
-    case 27:
-        /* Zavrsava se program. */
-        exit(0);
-        break;
+        case 'a':
+        case 'A':
+            if(anim_param1 > -0.95)
+                anim_param1 -= 0.05;
+            else
+                anim_param1 -= 0.0;
+            printf("1  %f\n", anim_param1);
+            glutPostRedisplay();
+            break;
+        case 'd':
+        case 'D':
+            if(anim_param1 < 0.95)
+                anim_param1 += 0.05;
+            else
+                anim_param1 += 0.0;
+            printf("1  %f\n", anim_param1);
+            glutPostRedisplay();
+            break;
+        case 's':
+        case 'S':
+            if(anim_param2 < 0.95)
+                anim_param2 += 0.05;
+            else
+                anim_param1 += 0.0;
+            printf("2  %f\n", anim_param2);
+            glutPostRedisplay();
+            break;
+        case 'w':
+        case 'W':
+            if(anim_param2 > -0.95)
+                anim_param2 -= 0.05;
+            else
+                anim_param2 -= 0.0;
+            printf("2  %f\n", anim_param2);
+            glutPostRedisplay();
+            break;
+        case 27:
+            /* Zavrsava se program. */
+            exit(0);
+            break;
         
     }
 }
@@ -69,84 +94,45 @@ static void on_reshape(int width, int height){
     /* Podesava se projekcija. */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60, (float) width / height, 1, 10);
+    gluPerspective(60, (float) width / height, 1, 50);
 }
 
-
-
-/* Izracunava vrednost funkcije za parametre u i v */
-float function(float u, float v){
-    return 0;
-}
-
-/* Postavlja normalu i kordinate verteksa za tacku sa pparametrima
- * u i v */
-void set_vertex_and_normal(float u, float v){
-    float diff_u, diff_v;
-    diff_u = 0;
-    diff_v = 0;
-    
-    /* Postavljamo normalu - vektor normalan na tangentnu ravan */
-    /* Racuna se priblizan vektor normale: */
-    glNormal3f(-diff_u, 1, -diff_v);
-
-    /* Postavljamo verteks */
-    /*glVertex3f(u, function(u, v), v);  ako treba fja*/ 
-    glVertex3f(u, 0, v);
-    
-}
-
-/* Crtamo funkciju */
+/* Crtamo ravan */
 void plot_function(){
-    float scale;
-    int u, v;
-
     glPushMatrix();
-
-    /* Racunamo faktor skaliranja tako da cela funkcija bude prikazana */
-    scale = 2.0 / max( U_TO - U_FROM, V_TO - V_FROM );
-    glScalef(scale, scale, scale);
-
-    /* Crtamo funkciju strip po strip */
-    for (u = U_FROM; u < U_TO; u++) {
-        glBegin(GL_TRIANGLE_STRIP);
-        for (v = V_FROM; v <= V_TO; v++) {
-            set_vertex_and_normal(u, v);
-            set_vertex_and_normal(u + 1, v);
-        }
-        glEnd();
-    }
+    glBegin(GL_POLYGON);
+        glVertex3f(-1, 0, -1);
+        glVertex3f(1, 0, -1);
+        glVertex3f(1, 0, 1);
+        glVertex3f(-1, 0, 1);
+    glEnd();
     glPopMatrix();
 }
 
 static void on_display(void){
-    /* Pozicija svetla (u pitanju je direkcionalno svetlo). */
+    /*Pozicija svetla (u pitanju je direkcionalno svetlo) */
     GLfloat light_position[] = { 1, 1, 1, 0 };
-    /* Ambijentalna boja svetla. */
+    /*Ambijentalna boja svetla */
     GLfloat light_ambient[] = { 0.1, 0.1, 0.1, 1 };
-    /* Difuzna boja svetla. */
+    /*Difuzna boja svetla */
     GLfloat light_diffuse[] = { 0.7, 0.7, 0.7, 1 };
-    /* Spekularna boja svetla. */
+    /*Spekularna boja svetla */
     GLfloat light_specular[] = { 0.9, 0.9, 0.9, 1 };
 
-    /* Koeficijenti ambijentalne refleksije materijala. */
     GLfloat ambient_coeffs[] = { 0.3, 0.8, 0.3, 1 };
-    /* Koeficijenti difuzne refleksije materijala. */
     GLfloat diffuse_coeffs[] = { 0.2, 0.5, 0.5, 1 };
-    /* Koeficijenti spekularne refleksije materijala. */
-    GLfloat specular_coeffs[] = { 1, 0, 0, 1 };
-    /* Koeficijent glatkosti materijala. */
+    GLfloat specular_coeffs[] = { 1, 0.1, 0.1, 1 };
     GLfloat shininess = 30;
 
-    /* Brise se prethodni sadrzaj prozora. */
+    /*Brise se prethodni sadrzaj prozora */ 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /* Podesava se vidna tacka. */
+    /*Podesava se vidna tacka */
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(-1.5, 1, 1.8, 0, 0, 0, 0, 1, 0);
 
-    /* Ukljucuje se osvjetljenje i podesavaju parametri svetla. */
+    /*Ukljucuje se osvjetljenje i podesavaju parametri svetla */ 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
@@ -154,7 +140,7 @@ static void on_display(void){
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 
-    /* Podesavaju se parametri materijala. */
+    /*Podesavaju se parametri materijala */ 
     glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
     glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
     glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
@@ -163,27 +149,47 @@ static void on_display(void){
     glShadeModel(GL_SMOOTH);
     
     /*X*/
-    glColor3f(1,0,0);
-        glBegin(GL_LINES);
-            glVertex3f(0, 0, 0);
-            glVertex3f(100, 0, 0);
-        glEnd();
+    
+    glBegin(GL_LINES);
+        glVertex3f(0, 0, 0);
+        glVertex3f(100, 0, 0);
+    glEnd();
     /*Y*/ 
-    glColor3f(0,1,0);
-        glBegin(GL_LINES);
-            glVertex3f(0, 0, 0);
-            glVertex3f(0, 100, 0);
-        glEnd();
+    
+    glBegin(GL_LINES);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 100, 0);
+    glEnd();
         
     /*Z*/
-    glColor3f(0,0,1);
-        glBegin(GL_LINES);
-            glVertex3f(0, 0, 0);
-            glVertex3f(0, 0, 100);
-        glEnd();
+    
+    glBegin(GL_LINES);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 0, 100);
+    glEnd();
 
-    /* Kreira se objekat. */
     plot_function();
+    
+    /*glColor3f(0, 0.5, 0.5);*/
+    glTranslatef(0, 0.05, 0);
+    
+    glPushMatrix();
+      diffuse_coeffs[0] = 0.5;
+      diffuse_coeffs[1] = 0.0;
+      
+      ambient_coeffs[0] = 0.5;
+      ambient_coeffs[1] = 0.0;
+      ambient_coeffs[2] = 0.5;
+      
+      glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
+      glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
+     
+      glTranslatef(0+anim_param1, 0, 0+anim_param2);
+      glutSolidCube(0.1);
+    glPopMatrix();
+    
+    glTranslatef(0, -0.05, 0);
+    
 
     /* Nova slika se salje na ekran. */
     glutSwapBuffers();
