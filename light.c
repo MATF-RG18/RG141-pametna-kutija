c48b2fab9e6de1af218b78ff469de195b6ffd5f5
leic25 <lea.bela.97@gmail.com>
2019-05-04 19:23:10 +0200

    ...

---
+++ light.c
@@ -0,0 +1,20 @@
+#include "light.h"
+
+
+void init_lights(){
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+}
+
+/*void set_material(){
+    
+     Podesavaju se parametri materijala
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+}*/
